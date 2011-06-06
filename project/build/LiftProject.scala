2676542d80e665428b7e620e15e49f5443f6020d
David Pollak <feeder.of.the.bears@gmail.com>
2011-06-06 09:09:42 -0700

    Updated to Scala 2.9 and Lift 2.4-M1

--- project/build/LiftProject.scala
+++ project/build/LiftProject.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
-  val liftVersion = "2.3-RC4"
+  val liftVersion = "2.4-M1"
 
   //val scalatoolsSnapshot = 
   // "Scala Tools Snapshot" at "http://scala-tools.org/repo-snapshots/"
@@ -14,7 +14,7 @@ class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
     "net.liftweb" %% "lift-webkit" % liftVersion % "compile->default",
     "org.mortbay.jetty" % "jetty" % "6.1.22" % "test->default",
     "junit" % "junit" % "4.5" % "test->default",
-    "org.scala-tools.testing" %% "specs" % "1.6.5" % "test->default",
+    "org.scala-tools.testing" %% "specs" % "1.6.8" % "test->default",
     "com.h2database" % "h2" % "1.2.138"
   ) ++ super.libraryDependencies
 }
